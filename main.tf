323b9d783c9255ca4cf6c03e382f9fba6c1aa4c1
Alexander Chetcuti <chetcuti.alex@gmail.com>
2023-06-24 17:27:39 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
-# Define the Azure provider
-provider "azurerm" {
-  features {}
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
 }
 
-# Create a new resource group
-resource "azurerm_resource_group" "develop" {
-  name     = "develop"
-  location = "West Europe"
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
 }
